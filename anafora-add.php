03d62cf0a8ca956b3f307cd618b86be4deb91ab3
khatziar <khatziar@sch.gr>
2025-04-21 23:12:47 +0300

    modified:   anafora-add.php 	modified:   anafora-edit.php 	modified:   code-anafora.php 	modified:   config/db.php 	modified:   event-add.php 	modified:   event-edit.php 	modified:   include/footer.php 	modified:   include/header.php 	modified:   include/navbar.php 	modified:   toDo

--- anafora-add.php
+++ anafora-add.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php 
     include('include/header.php'); 
     if (isset($_GET['event_id'])){
@@ -56,65 +55,4 @@
 <!--row-->
 </div>
 <!--container-->
-
-=======
-<?php 
-    include('include/header.php'); 
-    if (isset($_GET['event_id'])){
-        $event_id = $_GET['event_id']; 
-        }
-?>
-
-<div class="container">
-    <div class="row">
-        <div class="col-md-12 mt-4">
-            <div class="card">
-                <div class="card-header">
-                    <h3>Εισαγωγή Αναφορών
-                        <a href="index.php" class="btn btn-primary float-end">Αρχική Σελίδα</a>
-                    </h3>
-                    <div class="card-body">
-                        <form action="code-anafora.php" method="post" enctype="multipart/form-data" class="needs-validation">
-                            <input type="hidden" name="event_id" value="<?php echo $event_id?>" />
-                            <div class="mt-3">
-                                <label>Ημερομηνία δημοσίευσης</label>
-                                <input type="date" name="dimosieusi" class="form-control" placeholder="Δώσε ημερομηνία δημοσίευσης Αναφοράς" required>
-                            </div>
-                            <div class="mt-3">
-                                <label>Μέσο</label>
-                                <input type="text" name="meso" class="form-control" placeholder="Δώσε το Μέσο δημοσίευσης" required>
-                            </div>
-                            <div class="mt-3">
-                                <label>Τίτλος</label>
-                                <input type="text" name="title" class="form-control" placeholder="Δώσε Τίτλο της δημοσίευσης" required>
-                            </div>
-                            <div class="mt-3">
-                                <label>Σύνδεσμος</label>
-                                <input type="text" name="link" class="form-control" placeholder="Δώσε Σύνδεσμο της δημοσίευσης" required>
-                            </div>
-                            <div class="mt-3">
-                                <label>Τοπικός Σύνδεσμος Αρχείου</label>
-                                <input type="text" name="llink" class="form-control" placeholder="Δώσε Τοπικό σύνδεσμο του αρχείου">
-                            </div>
-                            <div class="form-group mt-3">
-                                <label for="formFile" class="form-label">Pdf Αρχείο</label>
-                                <input class="form-control" type="file" id="formFile" name="pdfFile" >
-                            </div>
-
-                            <div class="mt-3">
-                                <button type="submit" name="save-anafora-btn" class="btn btn-primary">Αποθήκευση</button>
-                            </div>
-                        </form> <!--form-->
-                </div>
-            </div>
-
-        </div>
-    </div>
-    <!--col-md-12 mt-4-->
-</div>
-<!--row-->
-</div>
-<!--container-->
-
->>>>>>> 87166af7471c7de14ce91da385b2353d3a243608
 <?php include('include/footer.php'); ?>
--- event-edit.php
+++ event-edit.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!-- κ με Σημειώσεις -->
 <?php
 include('include/header.php');
@@ -62,69 +61,4 @@ include('config/db.php');
 </div>
 <!--container-->
 
-=======
-<!-- κ με Σημειώσεις -->
-<?php
-include('include/header.php');
-include('config/db.php');
-?>
-<div class="container">
-    <div class="row">
-        <div class="col-md-12 mt-4">
-            <div class="card">
-                <div class="card-header">
-                    <h3>Επεξεργασία Γεγονότος
-                        <a href="index.php" class="btn btn-primary float-end">Αρχική Σελίδα</a>
-                    </h3>
-                    <div class="card-body">
-                        <?php
-                        if (isset($_GET['id'])) {
-                            $id = $_GET['id'];
-
-                            $query = 'SELECT * FROM events WHERE id = :ev_id LIMIT 1';
-                            $stmt = $pdo->prepare($query);
-                            $stmt->execute(['ev_id' => $id]);
-                            $Event = $stmt->fetch();
-                        }
-
-                        ?>
-                        <form action="code-event.php" method="post" class="needs-validation">
-                            <!--<input type="hidden" name="id" value="<?php echo $Event->$id ?>">-->
-                            <div class="mt-3">
-                                <label>id Γεγονότος</label>
-                                <input type="text" name="id" value="<?= $Event->id ?>" class="form-control" readonly>
-                            </div>
-                            <div class="mt-3">
-                                <label>Ημερομηνία</label>
-                                <input type="date" name="date" value="<?= $Event->date ?>" class="form-control" required>
-                            </div>
-                            <div class="mt-3">
-                                <label>Τοποθεσία</label>
-                                <input type="text" name="place" class="form-control" value="<?= $Event->place ?>" required>
-                            </div>
-                            <div class="mt-3">
-                                <label>Περιγραφή</label>
-                                <textarea name="perigrafi" class="form-control" rows="6" required><?= $Event->perigrafi ?></textarea>
-                            </div>
-                            <div class="mt-3">
-                                <label>Σημειώσεις</label>
-                                <textarea name="simeioseis" class="form-control" rows="6"><?= $Event->simeioseis ?></textarea>
-                            </div>
-                            <div class="mt-3">
-                                <button type="submit" name="update-event-btn" class="btn btn-primary">Αποθήκευση</button>
-                            </div>
-                        </form>
-                        <!--form-->
-                    </div>
-                </div>
-
-            </div>
-        </div>
-        <!--col-md-12 mt-4-->
-    </div>
-    <!--row-->
-</div>
-<!--container-->
-
->>>>>>> 87166af7471c7de14ce91da385b2353d3a243608
 <?php include('include/footer.php'); ?>
